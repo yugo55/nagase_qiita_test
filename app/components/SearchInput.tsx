8e4fad7e3a4a7e10a857ffcbb885e383183f107b
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-08-11 13:19:17 +0900

    検索条件として日付を追加

--- app/components/SearchInput.tsx
+++ app/components/SearchInput.tsx
@@ -2,15 +2,21 @@
 
 import { useState } from "react";
 import APIPopup from "@/app/components/APIPopup";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 type SearchInputProps = {
   setSearchText: (text: string) => void;
   setApiKey: (key: string) => void;
 };
 
-export default function SearchInput({ setSearchText, setApiKey }: SearchInputProps) {
+export default function SearchInput({
+  setSearchText,
+  setApiKey,
+}: SearchInputProps) {
   const [isPopupVisible, setPopupVisible] = useState(false);
   const [searchText, setInputSearchText] = useState("");
+  const [date, setDate] = useState<Date | null>(null);
 
   const togglePopup = () => {
     setPopupVisible(!isPopupVisible);
@@ -18,6 +24,10 @@ export default function SearchInput({ setSearchText, setApiKey }: SearchInputPro
 
   const handleSearch = () => {
     setSearchText(searchText);
+  };
+
+  const handleDateChange = (selectedDate: Date | null) => {
+    setDate(selectedDate)
   };
 
   return (
@@ -29,6 +39,14 @@ export default function SearchInput({ setSearchText, setApiKey }: SearchInputPro
         onChange={(e) => setInputSearchText(e.target.value)}
         placeholder="検索テキストを入力"
       />
+      <DatePicker
+        dateFormat="yyyy/MM/dd"
+        selected={date}
+        onChange={(date: Date | null) => handleDateChange(date)}
+        className="border-black border-b ml-2.5 w-28 h-8"
+        placeholderText="日付を選択"
+      />
+      <span>以降に作成された記事</span>
       <button
         className="h-8 w-24 rounded-full bg-green-400 block mx-2.5"
         onClick={handleSearch}
